a4af30dca68cc8de8d0452ade2bf05b9c588ecea
Phillip Søren Bonné <phil3170@edu.eal.dk>
2019-05-03 11:33:21 +0200

    Udgift udvidet

--- BelalTransport/Udgift.cs
+++ BelalTransport/Udgift.cs
@@ -10,5 +10,56 @@ namespace BelalTransport
     {
         public List<double> liste = new List<double>();
         //liste.Add();
+        public List<string> UdgiftFelt = new List<string>();
+        public List<double> udgiftsliste = new List<double>();
+        int row = 0;
+        int Udgiftsnummer = 0;
+        int column = 0;
+
+        public void AddExpense()
+        {
+            bool exit = true;
+            bool looping = true;
+            row = 0;
+            Console.SetCursorPosition(row, column);
+
+            while (exit)
+            {
+                looping = true;
+                row = 0;
+                Console.SetCursorPosition(row, column);
+                Console.SetCursorPosition(row + 5, column);
+                string input = Console.ReadLine();
+
+                if (input.Equals(string.Empty))
+                {
+                    exit = false;
+                }
+
+                else if (input is string)
+                {
+                    UdgiftFelt.Add(input);
+                    row = 30;
+
+                    while (looping)
+                    {
+                        Console.SetCursorPosition(row, column);
+                        input = Console.ReadLine();
+
+                        if (int.TryParse(input, out Udgiftsnummer))
+                        {
+                            udgiftsliste.Add(Udgiftsnummer);
+                            column += 1;
+                            looping = false;
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Virker ikke");
+                        }
+                    }
+                }
+            }
+        }
     }
 }
